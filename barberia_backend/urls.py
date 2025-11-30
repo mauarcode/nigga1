635e993070ff1494c71773ec983e764bd87ef739
mau <mau.arco1@gmail.com>
2025-11-29 18:18:17 -0600

    Fix django admin URL path

--- barberia_backend/urls.py
+++ barberia_backend/urls.py
@@ -25,7 +25,7 @@ def home(request):
 
 urlpatterns = [
     path('', home, name='home'),
-    path('admin/', admin.site.urls),
+    path('django-admin/', admin.site.urls),
     path('api/', include('usuarios.urls')),
 ]
 
